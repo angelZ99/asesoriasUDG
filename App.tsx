55f9f99edaedca13583c44ff72ea2124f6f6f119
FranciscoSaldivar21 <francisco.saldivar4081@alumnos.udg.mx>
2023-03-02 08:47:13 -0600

    App change
    Test

--- App.tsx
+++ App.tsx
@@ -90,7 +90,7 @@ const App = () => {
             <DebugInstructions />
           </Section>
           <Section title="Learn More">
-            Read the docs to discover what to do next:
+            Prueba de cambio
           </Section>
           <LearnMoreLinks />
         </View>
